d7afd31e7bfc66a13badb88dcee6e4dc46d88ec7
Pochi <nicopodchi@hotmail.com>
2025-02-10 20:53:58 -0300

    pseudo y listastrings typedef

---
+++ PROYECTO_TALLER_2025/1.Auxiliares/listaStrings.h
@@ -0,0 +1,13 @@
+#ifndef LISTASTRINGS_H_INCLUDED
+#define LISTASTRINGS_H_INCLUDED
+#include "strDinamico.h"
+
+typedef struct nodoL
+{
+    strings s;
+    nodoL * sig;
+} nodoLista;
+
+typedef nodoLista * listaStr
+
+#endif // LISTASTRINGS_H_INCLUDED
